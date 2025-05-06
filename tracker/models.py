a442c2a7d0bec7903bf0c737fae2db2895a4030b
sabari <sabarivishnu26@gmail.com>
2025-05-06 16:47:39 +0530

    Intial setup and databse configured, Model created

---
+++ tracker/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    category = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    type = models.CharField(max_length=10, choices=[('income', 'Income'), ('expense', 'Expense')])
+    description = models.TextField(blank=True, null=True)
+
+class Account(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bank_name = models.CharField(max_length=100)
+    account_number = models.CharField(max_length=50)
+    card_type = models.CharField(max_length=50, choices=[('debit', 'Debit'), ('credit', 'Credit'), ('upi', 'UPI')])
+    balance = models.DecimalField(max_digits=12, decimal_places=2)
